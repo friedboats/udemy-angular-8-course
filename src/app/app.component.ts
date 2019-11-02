bc0433179921851b40c692af69b133decda47fcd
Nick Bence <nick.bence@gmail.com>
2019-11-01 22:57:35 -0400

    complete assignment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,15 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
-  
+  username = '';
+
+  get usernameIsBlank() {
+    return this.username === '';
+  }
+
+  onClearInput() {
+    this.username = '';
+  }
 }
